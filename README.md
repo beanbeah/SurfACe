665de867391a4d16852296141e42192dee26574e
dbsqwerty123 <sean.seah0311@gmail.com>
2021-08-25 22:51:24 +0800

    Added Wiring, Added proper testing

---
+++ README.md
@@ -0,0 +1,5 @@
+Wiring Guide
+D0-D3 -- IR sensors
+D8 -- UV Relay
+
+IR sensor powered by transformer
--- techblazer.ino
+++ techblazer.ino
@@ -1,43 +1,61 @@
-#include <Wire.h> 
-#include <LiquidCrystal_I2C.h>
-
-LiquidCrystal_I2C lcd(0x27,16,2);
-
 #define PWMA 5
 #define DIRA 0
 #define PWMB 4
 #define DIRB 2
+#define UV D8
+#define IR1 D0
+#define IR2 D1
+#define IR3 D2
+#define IR4 D3
+
+#define DEBUG true
+
+void time_delay(uint8_t pin, int interval_in_seconds){
+    digitalWrite(pin,HIGH);
+    delay(interval*1000);
+    digitalWrite(pin,LOW);
+  }
+
+void motor_debug(uint8_t analog_pin, uint8_t digital_pin, int counts, bool reverse){
+    digitalWrite(digital_pin,1);
+    for (int i =0; i<counts; i++){
+        analogWrite(analog_pin, (1000/counts)*i);
+        delay(1500);
+        analogWrite(analog_pin,0);
+      }
+    if (reverse){
+    digitalWrite(digital_pin,-1);
+    for (int i =0; i<counts; i++){
+        analogWrite(analog_pin, (1000/counts)*i);
+        delay(1500);
+        analogWrite(analog_pin,0);
+      }
+     }
+  }
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
-  Serial.println();
-  Serial.println("Starting...");
-
-
-
-  Serial.println("Preparing motor...");
+  pinMode(DIRA, OUTPUT);
+  pinMode(PWMA, OUTPUT); 
   pinMode(DIRB, OUTPUT);
   pinMode(PWMB, OUTPUT);  
-
-  pinMode(D8, OUTPUT);
-  digitalWrite(D8, HIGH);
-  Serial.println("UV dying");
-  delay(10000);
-  digitalWrite(D8,LOW);
-
-  analogWrite(PWMB, 500);
-  digitalWrite(DIRB, 1);
-  delay(1000);
-  analogWrite(PWMB, 0);
-delay(1000);
-  analogWrite(PWMA, 500);
-  digitalWrite(DIRA, 1);
-  delay(1000);
-  analogWrite(PWMA, 0);
+  pinMode(UV,OUTPUT);
+  pinMode(IR1,OUTPUT);
+  pinMode(IR2,OUTPUT);
+  pinMode(IR3,OUTPUT);
+  pinMode(IR4,OUTPUT);
+  
+  if (DEBUG){
+    Serial.println("Debug");
+    Serial.println("Testing UV for 10 seconds");
+    time_delay(UV,10);
+    Serial.println("Testing Motor A");
+    motor_debug(PWMA,DIRA,5,true);
+    Serial.println("Testing Motor B");
+    motor_debug(PWMB,DIRB,5,true);
+    }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
