4f50aedbb978cb18136dfc5d6eb65c8fac077b93
Chen An Hong <cahchenanhong@gmail.com>
2021-08-30 18:15:40 +0800

    finished table tracking code

--- techblazer/techblazer.ino
+++ techblazer/techblazer.ino
@@ -7,6 +7,8 @@
 #define FL_IR D5
 #define BR_IR D6
 #define BL_IR D7
+#define MOVE_SPEED 50
+#define BRAKE_SPEED 15
 
 #define DEBUG true
 
@@ -37,17 +39,31 @@ bool isOnFloor(uint8_t pin) {
   return digitalRead(pin) == 0;
 }
 
+void debug() {
+  Serial.print(isOnFloor(FL_IR));
+  Serial.print(isOnFloor(FR_IR));
+  Serial.print(isOnFloor(BL_IR));
+  Serial.print(isOnFloor(BR_IR));
+  Serial.print("\n");
+}
+
+enum State{
+  ON_TABLE,
+  OFF_TABLE,
+  REVERSE,
+  TERMINATION_LAP,
+  TERMINATE
+};
+
 void setup() {
   Serial.begin(115200);
   
   // Motor setup
   Motor lMotor = {PWMB, DIRB, true};
-  setupMotor(lMotor);
   Motor rMotor = {PWMA, DIRA, true};
+  setupMotor(lMotor);
   setupMotor(rMotor);
 
-
-  Serial.print("fuck you");
   // uv and ir setup
 //  pinMode(UV,OUTPUT)
   pinMode(FL_IR, INPUT);
@@ -55,24 +71,114 @@ void setup() {
   pinMode(BL_IR, INPUT);
   pinMode(BR_IR, INPUT);
 
+  //table code
+  int turnDir = -1;
+  enum State state = ON_TABLE;
   while(true) {
-     Serial.print("fl, fr sensor: ");
-     Serial.print(isOnFloor(FL_IR));
-     Serial.print(isOnFloor(FR_IR));
-     Serial.print(isOnFloor(BL_IR));
-     Serial.print(isOnFloor(BR_IR));
-     Serial.print("\n");
-  
-     if(isOnFloor(FL_IR)) {
+    switch(state) {
+      case ON_TABLE:
+      {
+        if(isOnFloor(FL_IR)) {
+           moveMotor(lMotor, MOVE_SPEED);
+        }else {
+           moveMotor(lMotor, 0);
+        }
+        if(isOnFloor(FR_IR)) {
+           moveMotor(rMotor, MOVE_SPEED);
+        }else {
+           moveMotor(rMotor, 0);
+        }
+        delay(5);
+        if(!isOnFloor(FL_IR) && !isOnFloor(FR_IR)) state = OFF_TABLE;
+        break;
+      }
+      case OFF_TABLE:
+      {
+        turnDir = -turnDir;
+        // Reverse 
+        moveMotor(lMotor, -50);
+        moveMotor(rMotor, -50);
+        delay(500);
+        moveMotor(lMotor, 15);
+        moveMotor(rMotor, 15);
+        
+        // turn 90
+        moveMotor(lMotor, MOVE_SPEED*turnDir);
+        moveMotor(rMotor, -MOVE_SPEED*turnDir);
+        delay(910);
+        moveMotor(lMotor, -15*turnDir);
+        moveMotor(rMotor, 15*turnDir);
+        
+        // move fwd
         moveMotor(lMotor, 50);
-     }else {
-        moveMotor(lMotor, 0);
-     }
-     if(isOnFloor(FR_IR)) {
         moveMotor(rMotor, 50);
-     }else {
+//        delay(1000);
+        int i = 0;
+        bool offTable = false;
+        while(i < 1000) {
+          if(!isOnFloor(FL_IR) && !isOnFloor(FR_IR)) {
+            offTable = true;
+            break;
+          }
+          i += 5;
+          delay(5);
+        }
+        moveMotor(lMotor, -15);
+        moveMotor(rMotor, -15);
+        
+        if(offTable) {
+          state = TERMINATION_LAP;
+          break;
+        }else {
+          // turn 90
+          moveMotor(lMotor, MOVE_SPEED*turnDir);
+          moveMotor(rMotor, -MOVE_SPEED*turnDir);
+          delay(870);
+          moveMotor(lMotor, -15*turnDir);
+          moveMotor(rMotor, 15*turnDir);
+          state = ON_TABLE;
+          break;
+        }
+      }
+      case TERMINATION_LAP:
+      {
+        // Reverse 
+        moveMotor(lMotor, -50);
+        moveMotor(rMotor, -50);
+        delay(500);
+        moveMotor(lMotor, 15);
+        moveMotor(rMotor, 15);
+        
+        // turn 90
+        moveMotor(lMotor, MOVE_SPEED*turnDir);
+        moveMotor(rMotor, -MOVE_SPEED*turnDir);
+        delay(910);
+        moveMotor(lMotor, -15*turnDir);
+        moveMotor(rMotor, 15*turnDir);
+
+        while(isOnFloor(FL_IR) || isOnFloor(FR_IR)) {
+          if(isOnFloor(FL_IR)) {
+            moveMotor(lMotor, MOVE_SPEED);
+          }else {
+             moveMotor(lMotor, 0);
+          }
+          if(isOnFloor(FR_IR)) {
+             moveMotor(rMotor, MOVE_SPEED);
+          }else {
+             moveMotor(rMotor, 0);
+          }
+          delay(5);
+        }
+        state = TERMINATE;
+        break;
+      }
+      case TERMINATE:
+      {
+        moveMotor(lMotor, 0);
         moveMotor(rMotor, 0);
-     }
+        break;
+      }
+    }
     delay(5);
   }
 }
@@ -91,7 +197,7 @@ void loop() {
 //  digitalWrite(digital_pin,1);
 //  for (int i =0; i<counts; i++){
 //      analogWrite(analog_pin, (1000/counts)*i);
-//      delay(1500);
+//      delay(1MOVE_SPEED0);
 //      analogWrite(analog_pin,0);
 //    }
 //  if (reverse){
