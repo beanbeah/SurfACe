f38642d5cc9af3a1ad7a7c46355b3414eeda9ee4
dbsqwerty <sean.seah0311@gmail.com>
2021-10-19 15:58:02 +0800

    updates for new robot

--- techblazer/techblazer.ino
+++ techblazer/techblazer.ino
@@ -9,10 +9,10 @@ String header;
 #define PWMB 4
 #define DIRB 2
 #define UV D8
-#define FR_IR D0
-#define FL_IR D5
+#define FR_IR D7
+#define FL_IR D0
 #define BR_IR D6
-#define BL_IR D7 
+#define BL_IR D5
 #define MOVE_SPEED 50
 #define BRAKE_SPEED 15
 
@@ -30,16 +30,13 @@ void setupMotor(Motor motor) {
 }
 
 void moveMotor(Motor motor, int speed) {
-  if(motor.reversed) speed = -speed;
+  if (motor.reversed) speed = -speed;
   digitalWrite(motor.DIR_PIN, speed >= 1);
   analogWrite(motor.PIN, abs(speed));
 }
 
-//void get
-
 void setUV(bool state) {
-  digitalWrite(UV,state);
-    
+  digitalWrite(UV, state);
 }
 
 bool isOnFloor(uint8_t pin) {
@@ -54,7 +51,7 @@ void debug() {
   Serial.print("\n");
 }
 
-enum State{
+enum State {
   ON_TABLE,
   OFF_TABLE,
   REVERSE,
@@ -62,231 +59,217 @@ enum State{
   TERMINATE
 };
 
-void movedebug(){
-    Motor lMotor = {PWMB, DIRB, true};
-  Motor rMotor = {PWMA, DIRA, true};
-  setupMotor(lMotor);
-  setupMotor(rMotor);
-    digitalWrite(UV,HIGH);
-    delay(10000);
-    digitalWrite(UV,LOW);
-    moveMotor(lMotor, 50);
-    delay(2500);
-    moveMotor(lMotor, 0);
-    delay(1000);
-    moveMotor(rMotor, 50);
-    delay(2500);
-    moveMotor(rMotor, 0);
-    delay(1000);   
-  }
-
-
-
-bool lifted(){
+bool lifted() {
   return (digitalRead(FR_IR) && digitalRead(FL_IR) && digitalRead(BR_IR) && digitalRead(BL_IR));
 }
 
 
 void setup() {
   Serial.begin(115200);
-
-
-
   pinMode(FL_IR, INPUT);
   pinMode(FR_IR, INPUT);
   pinMode(BL_IR, INPUT);
   pinMode(BR_IR, INPUT);
+  pinMode(UV, OUTPUT);
+  //  setUV(true);
+  //  delay(10000);
   WiFi.softAP(ssid, password);
   IPAddress IP = WiFi.softAPIP();
   Serial.print("AP IP address: ");
   Serial.println(IP);
   server.begin();
-  
 }
 
-void loop(){
-  WiFiClient client = server.available(); 
-  if (client){
-    String currentLine = "";  
-     while (client.connected()){
-        if (client.available()){
-            char c = client.read(); 
-            header += c;
-            if (c == '\n') {  
-              if (currentLine.length() == 0) {
-                client.println("HTTP/1.1 200 OK");
-                client.println("Content-type:text/html");
-                client.println("Connection: close");
-                client.println();
+/**
+   TODO
+   1) May want to use a HTML File instead of spamming it here for code readability
+   2) Get start/stop button to actually work
+   3) Ensure Safety Features work (currently dont work)
+*/
 
-                if (header.indexOf("GET /start") >= 0){
-                    Serial.println("Start");
 
-                    client.println("<!DOCTYPE html><html>");
-                    client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-                    client.println("<link rel=\"icon\" href=\"data:,\">");
-                    client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
-                    client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
-                    client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
-                    client.println(".button2 {background-color: #555555;}</style></head>");
-    
-                    client.println("<body><h1>surfACe</h1>");
-                    client.println("<p><a href=\"/stop\"><button class=\"button\">STOP</button></a></p>");
-                    client.println("</body></html>");
-                    client.println();
-                      Motor lMotor = {PWMB, DIRB, true};
-                      Motor rMotor = {PWMA, DIRA, true};
-                      setupMotor(lMotor);
-                      setupMotor(rMotor);
 
-                    int turnDir = -1;
-                    enum State state = ON_TABLE;
-                    
 
-                    while (true){
+void loop() {
+  Motor lMotor = {PWMB, DIRB, true};
+  Motor rMotor = {PWMA, DIRA, false};
+  setupMotor(lMotor);
+  setupMotor(rMotor);
 
-                          switch(state) {
-      case ON_TABLE:
-      {
-        if(isOnFloor(FL_IR)) {
-           moveMotor(lMotor, MOVE_SPEED);
-        }else {
-           moveMotor(lMotor, 0);
-        }
-        if(isOnFloor(FR_IR)) {
-           moveMotor(rMotor, MOVE_SPEED);
-        }else {
-           moveMotor(rMotor, 0);
-        }
-        delay(5);
-        if(!isOnFloor(FL_IR) && !isOnFloor(FR_IR)) state = OFF_TABLE;
-        break;
-      }
-      case OFF_TABLE:
-      {
-        turnDir = -turnDir;
-        // Reverse 
-        moveMotor(lMotor, -50);
-        moveMotor(rMotor, -50);
-        delay(500);
-        moveMotor(lMotor, 15);
-        moveMotor(rMotor, 15);
-        
-        // turn 90
-        moveMotor(lMotor, MOVE_SPEED*turnDir);
-        moveMotor(rMotor, -MOVE_SPEED*turnDir);
-        delay(910);
-        moveMotor(lMotor, -15*turnDir);
-        moveMotor(rMotor, 15*turnDir);
-        
-        // move fwd
-        moveMotor(lMotor, 50);
-        moveMotor(rMotor, 50);
-//        delay(1000);
-        int i = 0;
-        bool offTable = false;
-        while(i < 1000) {
-          if(!isOnFloor(FL_IR) && !isOnFloor(FR_IR)) {
-            offTable = true;
-            break;
-          }
-          i += 5;
-          delay(5);
-        }
-        moveMotor(lMotor, -15);
-        moveMotor(rMotor, -15);
-        
-        if(offTable) {
-          state = TERMINATION_LAP;
-          break;
-        }else {
-          // turn 90
-          moveMotor(lMotor, MOVE_SPEED*turnDir);
-          moveMotor(rMotor, -MOVE_SPEED*turnDir);
-          delay(870);
-          moveMotor(lMotor, -15*turnDir);
-          moveMotor(rMotor, 15*turnDir);
-          state = ON_TABLE;
-          break;
-        }
-      }
-      case TERMINATION_LAP:
-      {
-        // Reverse 
-        moveMotor(lMotor, -50);
-        moveMotor(rMotor, -50);
-        delay(500);
-        moveMotor(lMotor, 15);
-        moveMotor(rMotor, 15);
-        
-        // turn 90
-        moveMotor(lMotor, MOVE_SPEED*turnDir);
-        moveMotor(rMotor, -MOVE_SPEED*turnDir);
-        delay(910);
-        moveMotor(lMotor, -15*turnDir);
-        moveMotor(rMotor, 15*turnDir);
+  int turnDir = -1;
+  enum State state = ON_TABLE;
+  setUV(false);
 
-        while(isOnFloor(FL_IR) || isOnFloor(FR_IR)) {
-          if(isOnFloor(FL_IR)) {
-            moveMotor(lMotor, MOVE_SPEED);
-          }else {
-             moveMotor(lMotor, 0);
-          }
-          if(isOnFloor(FR_IR)) {
-             moveMotor(rMotor, MOVE_SPEED);
-          }else {
-             moveMotor(rMotor, 0);
-          }
-          delay(5);
-        }
-        state = TERMINATE;
-        break;
-      }
-      case TERMINATE:
-      {
-        moveMotor(lMotor, 0);
-        moveMotor(rMotor, 0);
-        break;
-      }
-    }
-   
-                          delay(5);
+  WiFiClient client = server.available();
+  if (client) {
+    String currentLine = "";
+    while (client.connected()) {
+      Serial.println("Connected");
+      if (client.available()) {
+        char c = client.read();
+        header += c;
+        if (c == '\n') {
+          if (currentLine.length() == 0) {
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+
+
+            if (header.indexOf("GET /stop") >= 0) {
+              Serial.println("Stop");
+              moveMotor(lMotor, 0);
+              moveMotor(rMotor, 0);
+              setUV(false);
+              //              movedebug();
+            } else if (header.indexOf("GET /start") >= 0) {
+              Serial.println("Start");
+              client.println();
+              bool finished = false;
+              while (!finished) {
+                if (lifted()) {
+                  state = TERMINATE;
+                } else {
+                  setUV(true);
+                }
+                switch (state) {
+                  case ON_TABLE:
+                    {
+                      if (isOnFloor(FL_IR)) {
+                        moveMotor(lMotor, MOVE_SPEED);
+                      } else {
+                        moveMotor(lMotor, 0);
                       }
+                      if (isOnFloor(FR_IR)) {
+                        moveMotor(rMotor, MOVE_SPEED);
+                      } else {
+                        moveMotor(rMotor, 0);
+                      }
+                      delay(5);
+                      if (!isOnFloor(FL_IR) && !isOnFloor(FR_IR)) state = OFF_TABLE;
+                      break;
+                    }
+                  case OFF_TABLE:
+                    {
+                      turnDir = -turnDir;
+                      // Reverse
+                      moveMotor(lMotor, -50);
+                      moveMotor(rMotor, -50);
+                      delay(500);
+                      moveMotor(lMotor, 15);
+                      moveMotor(rMotor, 15);
 
-                  }
+                      // turn 90
+                      moveMotor(lMotor, MOVE_SPEED * turnDir);
+                      moveMotor(rMotor, -MOVE_SPEED * turnDir);
+                      delay(910);
+                      moveMotor(lMotor, -15 * turnDir);
+                      moveMotor(rMotor, 15 * turnDir);
 
-                else if (header.indexOf("GET /debug") >=0){
-                    Serial.println("Debug");
-                    movedebug();
-                  }
-               else {
+                      // move fwd
+                      moveMotor(lMotor, 50);
+                      moveMotor(rMotor, 50);
+                      //        delay(1000);
+                      int i = 0;
+                      bool offTable = false;
+                      while (i < 1000) {
+                        if (!isOnFloor(FL_IR) && !isOnFloor(FR_IR)) {
+                          offTable = true;
+                          break;
+                        }
+                        i += 5;
+                        delay(5);
+                      }
+                      moveMotor(lMotor, -15);
+                      moveMotor(rMotor, -15);
 
-                client.println("<!DOCTYPE html><html>");
-                client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-                client.println("<link rel=\"icon\" href=\"data:,\">");
-                client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
-                client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
-                client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
-                client.println(".button2 {background-color: #555555;}</style></head>");
+                      if (offTable) {
+                        state = TERMINATION_LAP;
+                        break;
+                      } else {
+                        // turn 90
+                        moveMotor(lMotor, MOVE_SPEED * turnDir);
+                        moveMotor(rMotor, -MOVE_SPEED * turnDir);
+                        delay(870);
+                        moveMotor(lMotor, -15 * turnDir);
+                        moveMotor(rMotor, 15 * turnDir);
+                        state = ON_TABLE;
+                        break;
+                      }
+                    }
+                  case TERMINATION_LAP:
+                    {
+                      // Reverse
+                      moveMotor(lMotor, -50);
+                      moveMotor(rMotor, -50);
+                      delay(500);
+                      moveMotor(lMotor, 15);
+                      moveMotor(rMotor, 15);
 
-                client.println("<body><h1>surfACe</h1>");
-                client.println("<p><a href=\"/start\"><button class=\"button\">START</button></a></p>");
-                client.println("<p><a href=\"/debug\"><button class=\"button\">DEBUG</button></a></p>");
-                client.println("</body></html>");
-                client.println();
-               }
-                break;
-              } else{
-                  currentLine = "";
-                } 
-            } else if (c != '\r') {
-                 currentLine += c;  
+                      // turn 90
+                      moveMotor(lMotor, MOVE_SPEED * turnDir);
+                      moveMotor(rMotor, -MOVE_SPEED * turnDir);
+                      delay(910);
+                      moveMotor(lMotor, -15 * turnDir);
+                      moveMotor(rMotor, 15 * turnDir);
+
+                      while (isOnFloor(FL_IR) || isOnFloor(FR_IR)) {
+                        if (isOnFloor(FL_IR)) {
+                          moveMotor(lMotor, MOVE_SPEED);
+                        } else {
+                          moveMotor(lMotor, 0);
+                        }
+                        if (isOnFloor(FR_IR)) {
+                          moveMotor(rMotor, MOVE_SPEED);
+                        } else {
+                          moveMotor(rMotor, 0);
+                        }
+                        delay(5);
+                      }
+                      finished = true;
+                      //                          setUV(false);
+                      state = TERMINATE;
+                      break;
+                    }
+                  case TERMINATE:
+                    {
+                      finished = true;
+                      setUV(false);
+                      moveMotor(lMotor, 0);
+                      moveMotor(rMotor, 0);
+                      break;
+                    }
+                }
+
+                delay(5);
               }
-           }
+
+            }
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+            client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
+            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+            client.println(".button2 {background-color: #555555;}</style></head>");
+
+            client.println("<body><h1>surfACe</h1>");
+            client.println("<p><a href=\"/start\"><button class=\"button\">START</button></a></p>");
+            client.println("<p><a href=\"/stop\"><button class=\"button\">STOP</button></a></p>");
+            client.println("</body></html>");
+            client.println();
+            break;
+          } else {
+            currentLine = "";
+          }
+        } else if (c != '\r') {
+          currentLine += c;
         }
-        header = "";
-        client.stop();
-        Serial.println("Client disconnected.");
-        Serial.println("");  
+      }
     }
+    header = "";
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
   }
+}
